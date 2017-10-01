7b79c0eeb674bb8d9bd1f7f8d9b594148fbef997
Maurício Mutte <hi@mauriciomutte.com>
2017-10-01 14:59:40 -0300

    Add JS

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const body = document.querySelector('.body')
+const separate = document.querySelectorAll('.separate')
+const btnChangeColor = document.querySelector('.changeColor')
+
+btnChangeColor.addEventListener('click', function() {
+  btnChangeColor.classList.toggle('changeColor--blue')
+  body.classList.toggle('body--blue')
+  for (var i = 0; i < 2; i++) {
+    separate[i].classList.toggle('separate--white')
+  }
+  if (btnChangeColor.innerHTML === 'Azul/Branco') {
+    btnChangeColor.innerHTML = 'Branco/Preto'
+  } else {
+    btnChangeColor.innerHTML = 'Azul/Branco'
+  }
+})
