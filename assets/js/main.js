feb6c97bdda453ab786482cfce3384511af6566c
Maurício Mutte <hi@mauriciomutte.com>
2017-10-01 18:33:32 -0300

    Fix separate change color

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const btnChangeColor = document.querySelector('.changeColor')
 btnChangeColor.addEventListener('click', function() {
   btnChangeColor.classList.toggle('changeColor--blue')
   body.classList.toggle('body--blue')
-  for (var i = 0; i < 2; i++) {
+  for (var i = 0; i < 4; i++) {
     separate[i].classList.toggle('separate--white')
   }
   if (btnChangeColor.innerHTML === 'Azul/Branco') {
