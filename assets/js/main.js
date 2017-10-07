21850bb2e350594697b30757538d43eeccde0ce3
Maurício Mutte <hi@mauriciomutte.com>
2017-10-06 21:12:22 -0300

    Add btn change language interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,23 @@
 const body = document.querySelector('.body')
 const separate = document.querySelectorAll('.separate')
 const btnChangeColor = document.querySelector('.changeColor')
+const btnChangeLanguage = document.querySelector('.changeLanguage')
+const title = document.querySelector('.title')
+
+console.log(title.value)
 
 btnChangeColor.addEventListener('click', function() {
   btnChangeColor.classList.toggle('changeColor--blue')
+  btnChangeLanguage.classList.toggle('changeLanguage--blue')
   body.classList.toggle('body--blue')
   for (var i = 0; i < 4; i++) {
     separate[i].classList.toggle('separate--white')
   }
-  if (btnChangeColor.innerHTML === 'Azul/Branco') {
-    btnChangeColor.innerHTML = 'Branco/Preto'
-  } else {
-    btnChangeColor.innerHTML = 'Azul/Branco'
+  if (page === 1) {
+    if (btnChangeColor.innerHTML === 'Azul/Branco') {
+      btnChangeColor.innerHTML = 'Branco/Preto'
+    } else {
+      btnChangeColor.innerHTML = 'Azul/Branco'
+    }
   }
 })
